bc3c5e010a0d467ebabcd7bab81672d69edad467
Søren Kastrup <311363@RTS-365.dk>
2022-10-07 19:47:47 +0200

    added three more tests

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -9,77 +9,114 @@ public class ProgramTests
     }
 
     [Fact]
-    public void QualityDecreasesBy1WhileSellinPositive() {
-        //Arrange
+    public void QualityDecreasesBy1WhileSellinPositive() 
+    {
+        // Arrange
         _items.Add(new Item{ Name = "Test Item", SellIn = 10, Quality = 20 });
-        //Act
+
+        // Act
         Program.UpdateQuality(_items);
-        //Assert
+
+        // Assert
         _items.ElementAt(0).Quality.Should().Be(19);
     }
 
     [Fact]
-    public void QualityDecreasesBy2WhileSellinNegative() {
+    public void QualityDecreasesBy2WhileSellinNegative() 
+    {
+        // Arrange
+        _items.Add(new Item{ Name = "Test Item", SellIn = -10, Quality = 20 });
+
+        // Act
+        Program.UpdateQuality(_items);
+
+        // Assert
+        _items.ElementAt(0).Quality.Should().Be(18);
+    }
+
+    [Fact]
+    public void BrieQualityIncreasesBy1WhileSellinPositive() 
+    {
+        // Arrange
+        _items.Add(new Item{ Name = "Aged Brie", SellIn = 10, Quality = 20 });
+
+        // Act
+        Program.UpdateQuality(_items);
+
+        // Assert
+        _items.ElementAt(0).Quality.Should().Be(21);
+    }
+
+    [Fact]
+    public void BrieQualityIncreasesBy2WhileSellinNegative() 
+    {
+        // Arrange
+        _items.Add(new Item{ Name = "Aged Brie", SellIn = -10, Quality = 20 });
+
+        // Act
+        Program.UpdateQuality(_items);
+
+        // Assert
+        _items.ElementAt(0).Quality.Should().Be(22);
+    }
+
+    [Fact]
+    public void BrieQuality50UpdateShouldNotIncreaseQuality() 
+    {
         throw new NotImplementedException();
     }
 
     [Fact]
-    public void BrieQualityIncreasesBy1WhileSellinPositive() {
+    public void BackstagePassIncreasesBy1WhileSellinBiggerThan10() 
+    {
         throw new NotImplementedException();
     }
 
     [Fact]
-    public void BrieQualityIncreasesBy2WhileSellinNegative() {
+    public void BackstagePassIncreasesBy2WhileSellinBetween10And5() 
+    {
         throw new NotImplementedException();
     }
 
     [Fact]
-    public void BrieQuality50UpdateShouldNotIncreaseQuality() {
+    public void BackstagePassIncreasesBy3WhileSellinSmallerThan5() 
+    {
         throw new NotImplementedException();
     }
 
     [Fact]
-    public void BackstagePassIncreasesBy1WhileSellinBiggerThan10() {
+    public void BackstagePassDropsTo0WhileSellinNegative() 
+    {
         throw new NotImplementedException();
     }
 
     [Fact]
-    public void BackstagePassIncreasesBy2WhileSellinBetween10And5() {
+    public void BackstagePassQuality50UpdateShouldNotIncreaseQuality() 
+    {
         throw new NotImplementedException();
     }
 
     [Fact]
-    public void BackstagePassIncreasesBy3WhileSellinSmallerThan5() {
+    public void SulfurasQualityStaysAt80() 
+    {
         throw new NotImplementedException();
     }
 
     [Fact]
-    public void BackstagePassDropsTo0WhileSellinNegative() {
+    public void SulfurasSellinDoesNotChange() 
+    {
         throw new NotImplementedException();
     }
 
     [Fact]
-    public void BackstagePassQuality50UpdateShouldNotIncreaseQuality() {
+    public void ConjuredDecreasesBy2WhileSellinPositive() 
+    {
         throw new NotImplementedException();
     }
 
     [Fact]
-    public void SulfurasQualityStaysAt80() {
-        throw new NotImplementedException();
-    }
-
-    [Fact]
-    public void SulfurasSellinDoesNotChange() {
-        throw new NotImplementedException();
-    }
-
-    [Fact]
-    public void ConjuredDecreasesBy2WhileSellinPositive() {
-        throw new NotImplementedException();
-    }
-
-    [Fact]
-    public void ConjuredDecreasesBy4WhileSellinNegative() {
+    public void ConjuredDecreasesBy4WhileSellinNegative() 
+    {
         throw new NotImplementedException();
     }
 }
